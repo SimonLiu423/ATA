8bffc41e008515143f4d26e86efe0ee7da71f131
SimonLiu <simonliu423@gmail.com>
2025-12-15 15:49:35 +0800

    Fix: improve feedback prompt

--- prompts/policy_feedback.py
+++ prompts/policy_feedback.py
@@ -1,5 +1,5 @@
 prompt = """
-We trained a RL policy using the provided reward function code and tracked the values of the individual components in the reward function as well as global policy metrics such as success rates and episode lengths after every {feedback_timestep_freq} timesteps and the maximum, mean, minimum values encountered:
+We trained a RL policy from scratch using the provided reward function code & hyperparameters and tracked the values of the individual components in the reward function as well as global policy metrics such as success rates and episode lengths after every {feedback_timestep_freq} timesteps and the maximum, mean, minimum values encountered:
 
 {reflection}
 """
