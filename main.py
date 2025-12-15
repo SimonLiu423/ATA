8f4950f925ecc00f1748d77f20429206a38b2fa2
SimonLiu <simonliu423@gmail.com>
2025-12-15 16:06:37 +0800

    Refactor: change environment to CartPole and update task description and action/observation spaces accordingly

--- main.py
+++ main.py
@@ -60,28 +60,51 @@ FEEDBACK_FREQ = TOTAL_TIMESTEPS // (N_ENVS * 10)
 RETRY_COUNT = 3
 # --------------------
 
-ENV_ID = "BipedalWalker-v3"
-ENV_KWARGS = {"hardcore": True}
+ENV_ID = "CartPole-v1"
+ENV_KWARGS = {}
 DEVICE = "cpu"
 TASK_DESC = """
 ## Task
-Your task is to train a 2D two-legged robot to coordinate its limbs and successfully walk from the left side of the screen to the right side without falling over
+Your task is to balance a pole upright on a moving cart for as long as possible.
+
+## Description
+This environment corresponds to the version of the cart-pole problem described by Barto, Sutton, and Anderson in “Neuronlike Adaptive Elements That Can Solve Difficult Learning Control Problem”. A pole is attached by an un-actuated joint to a cart, which moves along a frictionless track. The pendulum is placed upright on the cart and the goal is to balance the pole by applying forces in the left and right direction on the cart.
 
 ## Action Space
 __dict__: {action_space_dict}
 
-Actions are motor speed values in the [-1, 1] range for each of the 4 joints at both hips and knees.
+The action is a ndarray with shape (1,) which can take values {{0, 1}} indicating the direction of the fixed force the cart is pushed with.
+- 0: Push cart to the left
+- 1: Push cart to the right
+
+**Note**: The velocity that is reduced or increased by the applied force is not fixed and it depends on the angle the pole is pointing. The center of gravity of the pole varies the amount of energy needed to move the cart underneath it
 
 ## Observation Space
 __dict__: {observation_space_dict}
+The observation is a ndarray with shape (4,) with the values corresponding to the following positions and velocities:
 
-State consists of hull angle speed, angular velocity, horizontal speed, vertical speed, position of joints and joints angular speed, legs contact with ground, and 10 lidar rangefinder measurements. There are no coordinates in the state vector.
+| Num | Observation | Min | Max |
+| --- | --- | --- | --- |
+| 0 | Cart Position | -4.8 | 4.8 |
+| 1 | Cart Velocity | -Inf | Inf |
+| 2 | Pole Angle | ~ -0.418 rad (-24°) | ~ 0.418 rad (24°) |
+| 3 | Pole Angular Velocity | -Inf | Inf |
+
+**Note**: While the ranges above denote the possible values for observation space of each element, it is not reflective of the allowed values of the state space in an unterminated episode. Particularly:
+
+The cart x-position (index 0) can be take values between (-4.8, 4.8), but the episode terminates if the cart leaves the (-2.4, 2.4) range.
+
+The pole angle can be observed between (-.418, .418) radians (or ±24°), but the episode terminates if the pole angle is not in the range (-.2095, .2095) (or ±12°)
 
 ## Starting State
-The walker starts standing at the left end of the terrain with the hull horizontal, and both legs in the same position with a slight knee angle.
+All observations are assigned a uniformly random value in (-0.05, 0.05)
 
 ## Episode Termination
-The episode will terminate if the hull gets in contact with the ground or if the walker exceeds the right end of the terrain length.
+The episode ends if any one of the following occurs:
+
+- Termination: Pole Angle is greater than ±12°
+- Termination: Cart Position is greater than ±2.4 (center of the cart reaches the edge of the display)
+- Truncation: Episode length is greater than 500 (200 for v0)
 """
 
 
@@ -385,7 +408,7 @@ def train_baseline():
         ENV_ID,
         ENV_KWARGS,
         PPO,
-        {},
+        {"policy": "MlpPolicy"},
         N_ENVS,
         EurekaWrapper,
         {"is_eval": True},
@@ -531,7 +554,7 @@ async def train_eureka(main_agent: AgentTrainerAgent):
 
 async def main():
     # # Train baseline
-    # train_baseline()
+    train_baseline()
 
     train_config = TrainingConfig()
     agent = AgentTrainerAgent(train_config, "main_session")
